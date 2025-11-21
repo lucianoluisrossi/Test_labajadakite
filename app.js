67d1f9aaf2088fb962a51ec95811c96642c22eb8
lucianoluisrossi <lucianoluisrossi@gmail.com>
2025-11-21 20:11:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -456,7 +456,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function getUnifiedWindColorClasses(speedInKnots, degrees) {
-        /*if (degrees !== null && (degrees > 292.5 || degrees <= 67.5)) return ['bg-red-400', 'border-red-600'];*/
+        /*if (degrees !== null && (degrees > 292.5 || degrees <= 67.5)) return ['bg-red-400', 'border-red-600'];
         if (speedInKnots !== null && !isNaN(speedInKnots)) {
             if (speedInKnots <= 10) return ['bg-blue-200', 'border-blue-400']; 
             else if (speedInKnots <= 16) return ['bg-cyan-300', 'border-cyan-500']; 
@@ -465,7 +465,16 @@ document.addEventListener('DOMContentLoaded', () => {
             else if (speedInKnots <= 33) return ['bg-red-400', 'border-red-600']; 
             else return ['bg-purple-400', 'border-purple-600']; 
         }
-        return ['bg-gray-100', 'border-gray-300']; 
+        return ['bg-gray-100', 'border-gray-300']; */
+		if (speed === null) return ["Calculando...", ['bg-gray-100', 'border-gray-300']];
+        if (speed <= 14) return ["FLOJO...", ['bg-blue-200', 'border-blue-400']];
+        else if (speed <= 16) return ["ACEPTABLE", ['bg-cyan-300', 'border-cyan-500']];
+        else if (speed <= 19) return ["¡IDEAL!", ['bg-green-300', 'border-green-500']];
+        else if (speed <= 22) return ["¡MUY BUENO!", ['bg-yellow-300', 'border-yellow-500']];
+        else if (speed <= 27) return ["¡FUERTE!", ['bg-orange-300', 'border-orange-500']];
+        else if (speed > 33) return ["¡DEMASIADO FUERTE!", ['bg-purple-400', 'border-purple-600']];
+        else return ["¡MUY FUERTE!", ['bg-red-400', 'border-red-600']];
+		
     }
 
     function getWindyColorClasses(speedInKnots) {
