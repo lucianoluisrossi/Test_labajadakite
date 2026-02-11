f592c74854eb2aaa97035290fe1c1b1db787d8a4
lucianoluisrossi <lucianoluisrossi@gmail.com>
2026-02-11 11:48:30 -0300

    pu

--- app.js
+++ app.js
@@ -139,13 +139,6 @@ try {
             if (notifLoginRequired) notifLoginRequired.classList.add('hidden');
             if (notifSettingsLogged) notifSettingsLogged.classList.remove('hidden');
             
-            // Inicializar slider de viento mínimo después de mostrarlo
-            setTimeout(() => {
-                if (typeof window.initializeMinWindSlider === 'function') {
-                    window.initializeMinWindSlider();
-                }
-            }, 100);
-            
             console.log("✅ Usuario logueado:", user.displayName);
         } else {
             // Usuario no logueado
